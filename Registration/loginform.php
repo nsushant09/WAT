8aef01f451d85a4eb252a248960a5cbeaf3e4639
Sushant Neupane <nsushant09@gmail.com>
2022-12-25 20:28:03 +0545

    Completed registration

--- Registration/loginform.php
+++ Registration/loginform.php
@@ -5,39 +5,49 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles.css">
     <title>Login</title>
-    <style>
-        body{
-            box-sizing:content-box;
-        }
-    </style>
 </head>
 <body>
-    <form action="login.php" method="POST">
-        <fieldset>
-            <legend>Login</legend>
-            <label for="usernameLabel">Username:</label>
-            <input type="text" name="username">
-            <br>
-            <br>
-            <label for="passwordLabel">Password:</label>
-            <input type="password" name="password">
-            <br>
-            <br>
-            <input type="checkbox" name="chkRemember">
-            <label for="rememberMeLabel">Remember Me</label>
-            <br>
-            <br>
-            <label for="loginErrorLabel"><?php
+
+    <div class="container">
+        <div class="h1">
+            Login
+        </div>
+        <form action="login.php" method="POST">
+            <div class="mb-3">
+                <label for="usernameLabel" class="form-label">Username</label>
+                <input type="text" class="form-control" name="username">
+                <div id="usernameError" class="form-text"></div>
+            </div>
+
+            <div class="mb-3">
+                <label for="passwordLabel" class="form-label">Password</label>
+                <input type="text" class="form-control" name="password">
+                <div id="passwordError" class="form-text"></div>
+            </div>
+
+            <div class="mb-3">
+                <input type="checkbox" class="form-check-input" name="chkRemember">
+                <label for="chkRemember" class="form-check-label">Remember me</label>
+            </div>
+
+            <?php
                 if(isset($_SESSION['loginError'])){
-                    echo $_SESSION['loginError'];
-                }
-            ?></label>
-            <input type="submit" name="btnSubmit" value="Submit">
-            <input type="reset" name="btnReset" value="Clear">
-        </fieldset>
-    </form>
-    <p>New user? <a href="registration.php">Register</a></p>
+                    echo('<div class ="alert alert-danger" role="alert">' .$_SESSION['loginError'] .'</div>');
+                } 
+            ?>
+
+            <div class="mb-3">
+                <input type="submit" class="btn btn-primary" id="submitButtonID" value="Submit" name="btnSubmit">
+                <input type="reset" class="btn btn-outline-primary" id="cancelButtonID" value="Cancel" name="btnCancel">
+            </div>
+
+        </form>
+        
+        <div id="extrasNoteID">New user? <a href="registration.php">Register</a></p>
+
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
 </html>
--- Registration/registration.php
+++ Registration/registration.php
@@ -7,63 +7,65 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles.css">
     <title>Registration</title>
 </head>
 <body>
-    <p><?php
-        if(isset($_SESSION['registrationError'])){
-            echo $_SESSION['registrationError'];
-        }
-    ?></p>
-    <form action="register.php" method="POST">
-        <fieldset>
-            <legend>Register New User</legend>
-            <label for="usernameError"><?php
-                if(isset($_SESSION['usernameError'])){
-                    echo $_SESSION['usernameError'];
-                }
-            ?></label>
-            <label for="usernameLabel">Username:</label>
-            <input type="text" name="username" value="<?php
-                if(isset($_SESSION['username'])){
-                    echo $_SESSION['username'];
-                }
-            ?>">
-            <br>
-            <br>
-            <label for="emailError"><?php
-                if(isset($_SESSION['emailError'])){
-                    echo $_SESSION['emailError'];
-                }
-            ?></label>
-            <label for="emailLabel">Email:</label>
-            <input type="email" name="email" value="<?php
-                if(isset($_SESSION['email'])){
-                    echo $_SESSION['email'];
-                }
-            ?>">
-            <br>
-            <br>
-            <label for="passwordError"><?php
-                if(isset($_SESSION['passwordError'])){
-                    echo $_SESSION['passwordError'];
-                }
-            ?></label>
-            <label for="passwordLabel">Password:</label>
-            <input type="password" name="password" value="<?php
-                if(isset($_SESSION['password'])){
-                    echo $_SESSION['password'];
-                }
-            ?>">
-            <br>
-            <br>
-            <label for="ageRangeError"><?php
-                if(isset($_SESSION['ageRangeError'])){
-                    echo $_SESSION['ageRangeError'];
-                }
-            ?></label>
-            <label for="ageRangeLabel">Age :</label>
-            <select name="ageRange" id="ageRange">
+
+    <div class="container">
+        <?php
+            if(isset($_SESSION['registrationError'])){
+                echo('<div class ="alert alert-danger" role="alert">' .$_SESSION['registrationError'] .'</div>');
+            } 
+        ?>
+        <div class="h1">Register</div>
+        <form action="register.php" method="POST">
+
+            <div class="mb-3">
+                <label for="usernameLabel" class="form-label">Username</label>
+                <input type="text" class="form-control" name="username" value="<?php
+                    if(isset($_SESSION['username'])){
+                        echo $_SESSION['username'];
+                    }
+                    ?>">
+                <div id="usernameError" class="form-text"><?php
+                    if(isset($_SESSION['usernameError'])){
+                        echo $_SESSION['usernameError'];
+                    }
+                ?></div>
+            </div>
+
+            <div class="mb-3">
+                <label for="emailLabel" class="form-label">Email Address</label>
+                <input type="email" class="form-control" name="email" value="<?php
+                    if(isset($_SESSION['email'])){
+                        echo $_SESSION['email'];
+                    }
+                ?>">
+                <div id="emailError" class="form-text"><?php
+                    if(isset($_SESSION['emailError'])){
+                        echo $_SESSION['emailError'];
+                    }
+            ?></div>
+            </div>
+
+            <div class="mb-3">
+                <label for="passwordLabel" class="form-label">Password</label>
+                <input type="password" class="form-control" name="password" value="<?php
+                    if(isset($_SESSION['password'])){
+                        echo $_SESSION['password'];
+                    }?>">
+                <div id="passwordError" class="form-text"><?php
+                    if(isset($_SESSION['passwordError'])){
+                        echo $_SESSION['passwordError'];
+                    }
+                ?></div>
+            </div>
+
+            <div class="mb-3">
+                <label for="ageRangeLabel" class="form-label">Age</label>
+                <select class="form-select" name="ageRange" id="ageRange">
                 <option value="unselected"<?php
                     if(isset($_SESSION['ageRange']) && $_SESSION['ageRange'] == 'unselected') echo "SELECTED";
                 ?>>Select Age Range</option>
@@ -77,25 +79,38 @@
                     if(isset($_SESSION['ageRange']) && $_SESSION['ageRange'] == 'aged') echo "SELECTED";
                 ?>>60+</option>
             </select>
-            <br>
-            <br>
-            <input type="checkbox" name="chkTandC" <?php
-                if(isset($_SESSION['chkTandC'])){
-                    if($_SESSION['chkTandC'] == 'on') echo("CHECKED");
-                }
-            ?>>
-            <label for="termsAndConditionLabel">I agree to terms and conditions.</label>
-            <label for="termsAndConditionError"><?php
-                if(isset($_SESSION['chkTandCError'])){
-                    echo($_SESSION['chkTandCError']);
-                }
-            ?></label>
-            <br>
-            <br>
-            <input type="submit" value="Submit" name="btnSubmit">
-            <input type="reset" value="Cancel" name="btnCancel">
-        </fieldset>
-        <p>Already a user? <a href="loginform.php">Login</a></p>
-    </form>
+                <div id="ageRangeError" class="form-text"><?php
+                    if(isset($_SESSION['ageRangeError'])){
+                        echo $_SESSION['ageRangeError'];
+                    }
+            ?></div>
+            </div>
+
+            <div class="mb-3">
+                <input type="checkbox" class="form-check-input" name="chkTandC" value="<?php
+                    if(isset($_SESSION['chkTandC'])){
+                        if($_SESSION['chkTandC'] == 'on') echo("CHECKED");
+                    }
+                ?>">
+                <label for="chkTandCLabel" class="form-check-label">I agree the terms and conditions.</label>
+                <div id="chkTandCError" class="form-text"><?php
+                    if(isset($_SESSION['chkTandCError'])){
+                        echo $_SESSION['chkTandCError'];
+                    }
+                ?></div>
+            </div>
+
+            <div class="mb-3">
+                <input type="submit" class="btn btn-primary" id="submitButtonID" value="Submit" name="btnSubmit">
+                <input type="reset" class="btn btn-outline-primary" id="cancelButtonID" value="Cancel" name="btnCancel">
+            </div>
+
+        </form>
+
+        <div id="extrasNoteID">Already a user? <a href="loginform.php">Login</a></div>
+
+    </div>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
 </html>
